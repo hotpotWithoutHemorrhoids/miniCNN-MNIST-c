6bcfc4d780a3b751734036931598ef721056d507
hotpotWithoutHemorrhoids <123889414+hotpotWithoutHemorrhoids@users.noreply.github.com>
2025-01-16 01:01:53 +0800

    [cnn] add fc_test (#20)

--- tests/test_util.h
+++ tests/test_util.h
@@ -5,7 +5,7 @@
 int compare_float_arr(const float* arr1, const float* arr2, int size, float epsilon){
     for (size_t i = 0; i < size; i++){
         if(fabs(arr1[i] - arr2[i])>epsilon){
-            printf("i: %d is not equal arr1: %.2f, arr2: %.2f\n", i, arr1[i], arr2[i]);
+            printf("i: %d is not equal arr1: %f, arr2: %f\n", i, arr1[i], arr2[i]);
             return 0;// not equals
         }
             
